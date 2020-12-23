448e0c8ebfbf9c5e7cb6cd1352e43f0f03976339
jodafons <jodafons@lps.ufrj.br>
2020-12-23 19:09:28 +0000

    update ringer image

---
+++ ringer/setup_envs.sh
@@ -0,0 +1,21 @@
+
+echo "======================================================================================"
+echo "setup root..."
+# Set ROOT by hand
+export ROOTSYS="/opt/root/buildthis"
+export PATH="$ROOTSYS/bin:$PATH"
+export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH"
+export PYTHONPATH="/opt/root/buildthis/lib:$PYTHONPATH"
+
+echo "setup gaugi..."
+current=$PWD
+cd $(mktemp -d)
+temp=$PWD
+git clone https://github.com/ringer-atlas/gaugi.git
+cd $temp/gaugi && source scripts/setup.sh
+echo "======================================================================================"
+
+cd $current
+
+
+
